11a78066cf0ff3a7368adef81933d51bb036fbd2
Diego Urbina <diego.urbina1024@gmail.com>
2025-07-30 23:50:03 -0600

    feat: add in memory product repository

--- internal/product/repository.go
+++ internal/product/repository.go
@@ -1,10 +1,67 @@
 package product
 
 import (
+	"errors"
+
 	"github.com/rdurbina/stocky/internal/abstractions"
-	"github.com/rdurbina/stocky/internal/user"
 )
 
 type Repository interface {
-	abstractions.BaseRepository[user.Model]
+	abstractions.BaseRepository[Model]
+}
+
+/*
+In memory version of the product repository, it uses a map to keep track of product records,
+use only for development purposes
+*/
+type InMemoryRepository struct {
+	productCounter int
+	products       map[int]Model
+}
+
+/*InMemoryRepository methods*/
+
+// Retrives a product record from memory
+func (in InMemoryRepository) FindById(id int) (Model, error) {
+	value, ok := in.products[id]
+	if !ok {
+		return Model{}, errors.New("product not found")
+	}
+	return value, nil
+}
+
+// Saves a product record into volatile memory
+func (in *InMemoryRepository) Save(product Model) (Model, error) {
+	product.ID = in.productCounter
+	in.products[product.ID] = product
+	in.productCounter++
+	return product, nil
+}
+
+// Updates a product record saved in volatile memory
+func (in InMemoryRepository) Update(id int, product Model) (Model, error) {
+	_, ok := in.products[id]
+	if !ok {
+		return Model{}, errors.New("product not found")
+	}
+	in.products[id] = product
+	return product, nil
+
+}
+
+// Deletes a product from volatile memory
+func (in InMemoryRepository) Delete(id int) error {
+	_, ok := in.products[id]
+	if !ok {
+		return errors.New("product not found")
+	}
+	return nil
+}
+
+// Creates an instace of an in memory product repository, use only for development purposes
+func NewInMemoryRepository() Repository {
+	return &InMemoryRepository{
+		productCounter: 0,
+		products:       make(map[int]Model),
+	}
 }
